ded44bb9c68ab801314eb7a1633eadc63b38cfbb
Camila <caming2309@gmail.com>
2020-06-15 11:48:20 -0300

    finishing calculator

--- js/index.js
+++ js/index.js
@@ -1,31 +1,89 @@
-
+let runningTotal = 0;
+let previousOperator = null;
 let buffer = '0';
 const screen = document.querySelector('.screen');
 
 document.querySelector('.calculator-container').addEventListener('click', function (event) {
     buttonClick(event.target.innerText);
-    });
+});
 
-function buttonClick(value){
-    if(isNaN(parseInt(value))){
+
+// Qué hacer si apreto un número o un símbolo. 
+function buttonClick(value) {
+    if (isNaN(parseInt(value))) {
         handleSymbol(value);
     } else {
         handleNumber(value);
     }
+    reredender();
+
 }
 
-function handleNumber(value){
-    if(buffer === '0'){
+//Si apreto número
+function handleNumber(value) {
+    if (buffer === '0') {
         buffer = value;
     } else {
         buffer += value;
     }
-    reredender();
 }
 
-function handleSymbol(value){
+//Si apreto símbolo. 
+function handleSymbol(value) {
+    switch (value) {
+        case 'C':
+            buffer = '0';
+            runningTotal = 0;
+            previousOperator = null;
+            break;
+        case '=':
+            if (previousOperator === null) {
+                return;
+            }
+            flushOperation(parseInt(buffer));
+            previousOperator = null;
+            buffer = '' + runningTotal;
+            runningTotal = 0;
+            break;
+        case '←':
+            if (buffer.length === 1) {
+                buffer = '0';
+
+            } else {
+                buffer = buffer.substring(0, buffer.length - 1);
+            }
+            break;
+        default:
+            handleMath(value);
+            break;
+    }
 }
 
+function handleMath(value) {
+    const intBuffer = parseInt(buffer);
+    if (runningTotal === 0) {
+        runningTotal = intBuffer;
+    } else {
+        flushOperation(intBuffer);
+    }
+
+    previousOperator = value;
+    buffer = '0';
+
+}
+
+
+function flushOperation(intBuffer) {
+    if (previousOperator === '+') {
+        runningTotal += intBuffer;
+    } else if (previousOperator === '-') {
+        runningTotal -= intBuffer;
+    } else if (previousOperator === 'x') {
+        runningTotal *= intBuffer;
+    } else {
+        runningTotal /= intBuffer;
+    }
+}
 function reredender() {
     screen.innerText = buffer;
 }
